f90d0e3854d91d080f204b60dbc7ec2a6dca3f42
Nathan Luè <natlue98@gmail.com>
2025-11-02 00:38:53 +0100

    Added ts to reportview

--- code/insintesi/src/views/ReportView.vue
+++ code/insintesi/src/views/ReportView.vue
@@ -13,7 +13,7 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { ref, onMounted, computed } from "vue";
 
 import StanceReport from "../components/reports/StanceReport.vue";
