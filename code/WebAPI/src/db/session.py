332e7d789d9ca1905f93ca00df49c0b1db8bf869
DanielJMatt0 <danieljosefmatt@gmail.com>
2025-11-01 16:18:43 +0100

    AI integrated to webAPI

--- code/WebAPI/src/db/session.py
+++ code/WebAPI/src/db/session.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from config import DATABASE_URL
+from src.config import DATABASE_URL
 from .base import Base
 
 # SQLAlchemy engine initialization
--- code/WebAPI/src/main.py
+++ code/WebAPI/src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from src.routers import question,auth,team,user,answer
+from src.routers import question,auth,team,user,answer,analyze
 from src.db.session import init_db
 from src.sanitizer.sanitizer import SanitizerMiddleware
 
@@ -15,6 +15,7 @@ app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(team.router, prefix="/team", tags=["team"])
 app.include_router(user.router, prefix="/user", tags=["user"])
 app.include_router(answer.router, prefix="/answer", tags=["answer"])
+app.include_router(analyze.router, prefix="/analyze", tags=["analyze"])
 
 if __name__ == "__main__":
     import uvicorn
