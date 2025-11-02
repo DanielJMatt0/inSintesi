41d49349a0bbeef4c917276f5fc748f2e4ed37a0
Mosè Ferrazzini <mose.ferrazzini@students.bfh.ch>
2025-11-02 02:26:35 +0100

    changed frontend build config

--- code/insintesi/vite.config.ts
+++ code/insintesi/vite.config.ts
@@ -1,12 +1,28 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-});
+  build: {
+    // turn off minification (for readable debug builds)
+    minify: false,
+
+    // include source maps for debugging in browser devtools
+    sourcemap: true,
+
+    // emove hashed filenames for easier inspection
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
+      },
+    },
+  },
+}))
